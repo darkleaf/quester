a167514e651f739bdba3a9f09becd4182d034175
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-08-13 16:13:19 +0400

    refactor slider

--- frontend/slider/component.js
+++ frontend/slider/component.js
@@ -6,19 +6,23 @@ export default class Slider extends Component {
     const columns = React.Children.count(this.props.children);
     return (
       <div className={styles.container}>
+
         <div className={styles.header}>
           <div>{this.props.title}</div>
         </div>
+
         <div className={styles.content}>
-          <div className={styles.left}>&lt;</div>
+          <div className={styles.leftArrow}>&lt;</div>
+
           <div className={styles.list}>
             {React.Children.map(this.props.children, (child) => {
-              return <div className={styles['item-' + columns]}>
+              return <div className={styles[`item-${columns}`]}>
                 {child}
               </div>
             })}
           </div>
-          <div className={styles.right}>&gt;</div>
+
+          <div className={styles.rightArrow}>&gt;</div>
         </div>
       </div>
     )
