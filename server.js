5efc0c40aece3e5754d27c946e290d0e1c36712b
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-08-14 16:03:50 +0400

    fix host

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 var port = process.env.PORT || 3000;
+var host = process.env.PORT ? '0.0.0.0' : 'localhost';
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(port, 'localhost', function (err, result) {
+}).listen(port, host, function (err, result) {
   if (err) {
     return console.log(err);
   }
 
-  console.log('Listening at http://localhost:' + port);
+  console.log('Listening at http://' + host + ':' + port);
 });
