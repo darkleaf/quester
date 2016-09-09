d0fa371fca17ced64222393e58eebae7caeb0204
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-09-10 00:49:00 +0400

    save

--- src/clj/quester/web.clj
+++ src/clj/quester/web.clj
@@ -1,17 +1,8 @@
 (ns quester.web
  (:require [ring.adapter.jetty :refer [run-jetty]]
-           [mount.core :refer [defstate] :as mount]
-           [domkm.silk :as silk]
-           [quester.web.common-routes :as common-routes]))
-
-(def all-routes
-  (silk/routes [common-routes/routes]))
+           [mount.core :refer [defstate] :as mount]))
 
 
-;;TODO: add request-methods
-(defmulti handler
-  (fn [_ params]
-    (if-let [params (silk/arrive all-routes)])))
 
 (defn app-handler [req])
 
--- src/cljc/quester/web/common-routes.cljc
+++ src/cljc/quester/web/common-routes.cljc
@@ -1,7 +1,1 @@
-(ns quester.web.common-routes
-  (:require [domkm.silk :as silk]))
-
-(def routes
-  (silk/routes {:root [[]]
-                :quests [["quests"]]
-                :quest [["quests" (silk/int :id)]]}))
+(ns quester.web.common-routes)
