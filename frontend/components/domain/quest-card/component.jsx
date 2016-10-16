eea6126760deecc49181a9246edc38b0a5d15cc7
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-10-16 22:31:25 +0400

    icon

--- frontend/components/domain/quest-card/component.jsx
+++ frontend/components/domain/quest-card/component.jsx
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
-import { FormattedNumber } from 'react-intl';
+import Price from '../../widgets/price';
+import Icon from '../../widgets/icon';
+import Rating from '../../widgets/rating';
 import styles from './styles.css';
-
-function Price({ min, max }) {
-  return (
-    <div className={styles.price}>
-      <FormattedNumber
-        value={min}
-        style="currency"
-        currency="rub"
-        currencyDisplay="symbol"
-        maximumFractionDigits={0}
-      />
-      &nbsp;&mdash;&nbsp;
-      <FormattedNumber
-        value={max}
-        style="currency"
-        currency="rub"
-        currencyDisplay="symbol"
-        maximumFractionDigits={0}
-      />
-    </div>
-  );
-}
-
-Price.propTypes = {
-  min: React.PropTypes.number.isRequired,
-  max: React.PropTypes.number.isRequired,
-};
 
 function Participants({ min, max }) {
   return (
-    <div className={styles.participants}>
-      {min}&mdash;{max}
+    <div>
+      <Icon>
+        {min}&mdash;{max}
+      </Icon>
     </div>
   );
 }
@@ -40,18 +17,6 @@ function Participants({ min, max }) {
 Participants.propTypes = {
   min: React.PropTypes.number.isRequired,
   max: React.PropTypes.number.isRequired,
-};
-
-function Rating({ value }) {
-  return (
-    <div className={styles.rating}>
-      <FormattedNumber value={value} minimumFractionDigits={0} maximumFractionDigits={1} />
-    </div>
-  );
-}
-
-Rating.propTypes = {
-  value: React.PropTypes.number.isRequired,
 };
 
 export default class QuestCard extends Component {
