09e3025b642245e915249b626064c3a2df360f47
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-12-20 21:51:01 +0400

    webpack build config

---
+++ src/frontend-dev/webpack.config.build.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const postcssImport = require('postcss-import');
+const autoprefixer = require('autoprefixer');
+const use = require('postcss-use');
+
+const rootPath = path.join(__dirname, '..', '..');
+
+module.exports = {
+  entry:  path.join(rootPath, 'src', 'frontend', 'index'),
+  output: {
+    path: path.join(rootPath, 'resources', 'public', 'frontend'),
+    filename: 'bundle.js',
+    library: 'frontend',
+    libraryTarget: 'var',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(rootPath, 'src', 'frontend'),
+    }, {
+      test: /\.css$/,
+      loaders: [
+        'style-loader',
+        'css?modules&camelCase',
+        'postcss'],
+    }],
+  },
+  postcss() {
+    return [postcssImport, use({ modules: '*' }), autoprefixer];
+  },
+};
