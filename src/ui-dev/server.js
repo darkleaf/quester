bb66467538dac4a6a2ee11c30ba6f3b94e2e03a2
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2016-12-27 23:34:41 +0400

    move index.html

--- src/ui-dev/server.js
+++ src/ui-dev/server.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 const port = 3001;
 const host = 'localhost';
+const rootPath = path.join(__dirname, '..', '..');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
+  contentBase: path.join(rootPath, 'src', 'ui-dev', 'public'),
   hot: true,
   historyApiFallback: true,
 }).listen(port, host, function (err, result) {
