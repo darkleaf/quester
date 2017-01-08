665bb57a1370f91bb8761b790a82eff301353030
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2017-01-08 13:20:56 +0400

    remove unused

--- src/cljs/quester/containers/right_nav_section.cljs
+++
@@ -1,8 +0,0 @@
-(ns quester.containers.right-nav-section
-  (:require [quester.react :refer [e]]))
-
-(defn container []
-  (e js/ui.NavSection nil
-     (e js/ui.NavItem #js {:name "Москва"})
-     (e js/ui.NavItem #js {:name "Избранное"})
-     (e js/ui.NavItem #js {:name "Выйти"})))
