4872756caa04487ac7527a71c23e172f77d67b72
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2017-01-09 22:17:25 +0400

    gallery logic

--- src/ui/components/layout/gallery/component.jsx
+++ src/ui/components/layout/gallery/component.jsx
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './styles.css';
 
 export default class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      offset: 0,
+    };
+
+    this.handleLeftArrowClick = this.handleLeftArrowClick.bind(this);
+    this.handleRightArrowClick = this.handleRightArrowClick.bind(this);
+    this.windowLength = 3;
+  }
+
+  getWindow() {
+    const { offset } = this.state;
+    const count = this.props.imageUrls.length;
+    const leftPart = this.props.imageUrls.slice(offset, offset + this.windowLength);
+    const rightPart = this.props.imageUrls.slice(0, this.windowLength - leftPart.length);
+    return leftPart.concat(rightPart);
+  }
+
+  handleLeftArrowClick() {
+    const { offset } = this.state;
+    const count = this.props.imageUrls.length;
+    const newOffset = offset === 0 ? count - 1 : offset - 1;
+    this.setState({ offset: newOffset });
+  }
+
+  handleRightArrowClick() {
+    const { offset } = this.state;
+    const count = this.props.imageUrls.length;
+    const newOffset = offset === count - 1 ? 0 : offset + 1;
+    this.setState({ offset: newOffset });
+  }
+
   render() {
-    const { left, center, right } = this.props;
+    const [left, center, right] = this.getWindow();
+
     return (
       <div className={styles.container}>
         <div className={styles.sections}>
@@ -12,9 +46,15 @@ export default class Gallery extends Component {
           </div>
 
           <div className={styles.centerSection}>
-            <div className={styles.leftArrow} />
+            <div
+              onClick={this.handleLeftArrowClick}
+              className={styles.leftArrow}
+            />
             <img src={center} />
-            <div className={styles.rightArrow} />
+            <div
+              onClick={this.handleRightArrowClick}
+              className={styles.rightArrow}
+            />
           </div>
 
           <div className={styles.rightSection}>
@@ -27,7 +67,5 @@ export default class Gallery extends Component {
 }
 
 Gallery.propTypes = {
-  left: React.PropTypes.string,
-  center: React.PropTypes.string.isRequired,
-  right: React.PropTypes.string,
+  imageUrls: PropTypes.arrayOf(PropTypes.string),
 };
