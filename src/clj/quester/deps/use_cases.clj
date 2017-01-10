bcffc4e04551c071cfda31dcfd43ab871149569e
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-01-10 21:51:39 +0400

    reafactor container (#51)

--- src/clj/quester/deps/use_cases.clj
+++ src/clj/quester/deps/use_cases.clj
@@ -3,16 +3,20 @@
             [quester.use-cases.welcome :as welcome]
             [quester.use-cases.quests.show :as quests-show]))
 
-(c/register :use-cases/welcome
-            (fn [resolve]
-              (partial welcome/use-case
-                       {:select-selections-cards  (resolve :db.selections/cards)
-                        :select-best-quests-cards (resolve :db.quests/best-cards)
-                        :select-reviews-cards     (resolve :db.reviews/cards)
-                        :select-new-quests-cards  (resolve :db.quests/new-cards)
-                        :select-companies-cards   (resolve :db.companies/cards)})))
+(defn register [registry]
+  (c/register
+   registry
 
-(c/register :use-cases.quests/show
-            (fn [resolve]
-              (partial quests-show/use-case
-                       {:find-quest-page (resolve :db.quests/page)})))
+   :use-cases/welcome
+   (fn [resolve]
+     (partial welcome/use-case
+              {:select-selections-cards  (resolve :db.selections/cards)
+               :select-best-quests-cards (resolve :db.quests/best-cards)
+               :select-reviews-cards     (resolve :db.reviews/cards)
+               :select-new-quests-cards  (resolve :db.quests/new-cards)
+               :select-companies-cards   (resolve :db.companies/cards)}))
+
+   :use-cases.quests/show
+   (fn [resolve]
+     (partial quests-show/use-case
+              {:find-quest-page (resolve :db.quests/page)}))))
--- src/clj/quester/web_server.clj
+++ src/clj/quester/web_server.clj
@@ -6,13 +6,21 @@
             [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.content-type :refer [wrap-content-type]]
-            [ring.middleware.not-modified :refer [wrap-not-modified]]))
+            [ring.middleware.not-modified :refer [wrap-not-modified]]
+            [quester.container :as container]
+            [quester.deps :as deps]))
 
+(defn wrap-resolve [handler]
+  (fn [req]
+    (-> req
+        (assoc :resolve #(container/resolve deps/registry %))
+        (handler))))
 
 (def handler (-> (r/make-handler routes)
                  (wrap-resource "public")
                  (wrap-content-type)
-                 (wrap-not-modified)))
+                 (wrap-not-modified)
+                 (wrap-resolve)))
 
 (defstate web-server
   :start (run-jetty handler {:port 3000, :join? false})
