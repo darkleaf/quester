37c62a8bb51cfc3546726d7e6fef7257dc8a3f47
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2017-02-04 00:47:16 +0400

    fix

--- src/clj/quester/middlewares/web.clj
+++ src/clj/quester/middlewares/web.clj
@@ -19,7 +19,6 @@
     [:div#root]
     [:script#initial-data {:type "application/transit+json"}
      (data->transit data)]
-    #_(h/include-js "/ui/bundle.js")
     (h/include-js "/js/web.js")]))
 
 (defn middleware [handler]
