64f326c981df55f0fe46ef1981e4cbe4449bae12
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2017-03-14 08:57:15 +0400

    di->ctx (#57)
    * initial
    
    * backend on ctx
    
    * fix tests

--- test/clj/quester/requests/web/main_test.clj
+++ test/clj/quester/requests/web/main_test.clj
@@ -1,5 +1,31 @@
 (ns quester.requests.web.main-test
-  (:require [clojure.test :refer [deftest is]]
-            [quester.test-utils.request :refer [testing-page]]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [ring.mock.request :as mock]
+            [ring.util.http-predicates :as http-predicates]
+            [darkleaf.router :as r]
+            [quester.routes.core :refer [build-routes]]
+            [quester.use-cases.protocols.queries :as queries]))
 
-(deftest show (testing-page "/"))
+(deftest main
+  (testing :show
+    (let [handler (r/make-handler (build-routes))
+          ctx (reify
+                queries/GetNewQuestsCards
+                (-get-new-quests-cards [_ limit]
+                  [])
+                queries/GetBestQuestsCards
+                (-get-best-quests-cards [_ limit]
+                  [])
+                queries/GetCompnaiesCards
+                (-get-companies-cards [_ limit]
+                  [])
+                queries/GetSelectionsCards
+                (-get-selections-cards [_ limit]
+                  [])
+                queries/GetReviewsCards
+                (-get-reviews-cards [_ limit]
+                  []))
+          req (-> (mock/request :get "/")
+                  (assoc :quester/ctx ctx))
+          resp (handler req)]
+      (is (http-predicates/success? resp)))))
