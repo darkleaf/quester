b0e3894738bf6a9b273677801c35b7da8c25d02d
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2017-03-17 22:46:51 +0400

    welcome -> main

--- src/clj/quester/controllers/web/main.clj
+++ src/clj/quester/controllers/web/main.clj
@@ -1,10 +1,10 @@
 (ns quester.controllers.web.main
   (:require [ring.util.http-response :as response]
             [darkleaf.router :as r]
-            [quester.use-cases.welcome :as welcome]))
+            [quester.use-cases.main :refer [use-case]]))
 
 (r/defcontroller controller
   (show [req]
     (let [ctx (:quester/ctx req)
-          data (welcome/use-case ctx)]
+          data (use-case ctx)]
       (response/ok data))))
